b0eadc1768ed0134fbcd58b91b7fe2f3c3abf4f0
Ariel1997 <life23913349@gmail.com>
2022-07-29 00:24:50 +0800

    fix:header change

--- ariel-react-project/src/api/setup.js
+++ ariel-react-project/src/api/setup.js
@@ -3,7 +3,7 @@ const service = axios.create({
     baseURL: "https://g1api.finlogix.com/v1",  
 })
 const addConfigBeforeRequest = (config) => {
-    config.headers = { ...config.headers, Authorization: sessionStorage.getItem("token"),["Content-Type"]: "application/x-www-form-urlencoded"}
+    config.headers = { ...config.headers, Authorization: sessionStorage.getItem("token")}
     return config;
 }
 service.interceptors.request.use(
