66cf4155ef2bfe2ab4fb7ba9e3daa68d5f7ca77d
ariel_tsai <ariel_tsai@asgard.com.tw>
2022-07-28 17:54:28 +0800

    feat:分頁功能

--- ariel-react-project/src/api/api.js
+++ ariel-react-project/src/api/api.js
@@ -1,2 +1,3 @@
 import api from "../api/setup.js";
 export const auth = (body)=>{return api.post("https://g1api.finlogix.com/v1/auth/login/email",body)}
+export const getUnLoginList = (params)=>{return api.get("https://g1api.finlogix.com/v1/post/analysis",params)}
--- ariel-react-project/src/pages/Homepage.js
+++ ariel-react-project/src/pages/Homepage.js
@@ -1,119 +1,142 @@
+import { useState, useEffect } from "react";
+import { getUnLoginList } from "../api/api.js";
 function Homepage() {
-    return(
-  <div>
-        <div className="content-details">
-            <div className="details-inner">
-                <div className="inner-title"><span>Forex Webinars</span> </div>
-                <div className="inner-content">
-                <span>Whether you are new to foreign exchange trading or already have some market experience, we </span>
-                <span>believe that a solid FX trading education is vital to your success as a trader.</span>
-            </div>
-            </div>
-        </div>
- 
-        <div className="content-informations">
-            <div className="cards-group">
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-                <div className="card-item">
-                    <div className="card-item-details-date">Thursday 9th Oct</div>
-                    <div className="card-item-details-topic">NFP Preview plus Bank Of Canada</div>
-                    <div className="card-item-details-contents">A fundamental update getting across the key risk events which create trading opportunities.</div>
-                    <div className="card-item-details-time">7pm-8:30pm EST</div>
-                    <div className="card-item-details-bottom">
-                        <div className="card-item-details-bottom-registry">Register Now</div>
-                        <div className="card-item-details-bottom-icon"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  const [webinarsList, setWebinarsList] = useState([]);
+  const [webinarsAllList, setwebinarsAllList] = useState([]);
+  const [page, setPage] = useState(1);
 
-        <div className="content-registry">
-            <div className="registry-area">
-                <div className="registry-form">
-                    <div className="registry-header">
-                        <div className="registry-header-title"><span>Register for a Webinar now</span></div>
-                        <div className="registry-header-subtitle">
-                            <span>Please fill in the form below and you will be contacted by one of our</span>
-                            <span>professional business experts.</span>
-                        </div>
-                    </div>
-                    <div className="registry-submit">
-                        <div className="form-input">
-                            <div className="form-input-title">Topic</div>
-                            <div className="form-input-field"><select><option></option></select></div>                           
-                        </div>
-                        <div className="form-input required-field">
-                            <div className="form-input-title">First Name</div>
-                            <div className="form-input-field"><input type="text"/></div>
-                        </div>
-                        <div className="form-input required-field">
-                            <div className="form-input-title">Last Name</div>
-                            <div className="form-input-field"><input type="text"/></div>
-                        </div>
-                        <div className="form-input required-field">
-                            <div className="form-input-title">Email</div>
-                            <div className="form-input-field"><input type="text"/></div>
-                        </div>
-                        <div className="form-input">
-                            <div className="form-input-field">
-                                <button>Register</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      //
+    } else {
+      fetchDataUnLogin();
+    }
+  }, []);
+  
+  const fetchDataUnLogin = async () => {
+    let params = { params: { per_page: 12, page: 1 } };
+    let res = await getUnLoginList(params);
+    let resList = res.data.data.map((card)=>{
+        card.content=JSON.parse(card.content).blocks.reduce((acc,cur)=>{
+            acc=acc+cur.text
+            return acc
+        },"")
+        return card;
+    });
+    setwebinarsAllList(resList);
+    let showList=resList.slice(0,6)
+    setWebinarsList(showList);
+    // console.log(webinarsList);
+  };
+  const plus=()=>{
+    setPage(page+1);
+    let nextPage=webinarsAllList.slice((page-1)*6,(page-1)*6+6);
+    setWebinarsList(nextPage);
+  }
+  const minus=()=>{
+    setPage(page-1);
+    let prevPage=webinarsAllList.slice((page-1)*6,(page-1)*6+6);
+    setWebinarsList(prevPage);
+  }
+  return (
+    <div>
+      <div className="content-details">
+        <div className="details-inner">
+          <div className="inner-title">
+            <span>Forex Webinars</span>
+          </div>
+          <div className="inner-content">
+            <span>
+              Whether you are new to foreign exchange trading or already have
+              some market experience, we{" "}
+            </span>
+            <span>
+              believe that a solid FX trading education is vital to your success
+              as a trader.
+            </span>
+          </div>
         </div>
+      </div>
+
+      <div className="content-informations">
+        <div className="cards-group">
+          {webinarsList.map((card, index) => {
+            return (
+              <div className="card-item" key={`card_${index}`}>
+                <div className="card-item-details-date">{card.created_at.split(" ")[0]}</div>
+                <div className="card-item-details-topic">
+                {card.title}
+                </div>
+                <div className="card-item-details-contents">
+                    {card.content}
+                </div>
+                <div className="card-item-details-time">7pm-8:30pm EST</div>
+                <div className="card-item-details-bottom">
+                  <div className="card-item-details-bottom-registry">
+                    Register Now
+                  </div>
+                  <div className="card-item-details-bottom-icon"></div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={minus}>prev</button>
+        <button onClick={plus}>next</button>
+      </div>
+
+      <div className="content-registry">
+        <div className="registry-area">
+          <div className="registry-form">
+            <div className="registry-header">
+              <div className="registry-header-title">
+                <span>Register for a Webinar now</span>
+              </div>
+              <div className="registry-header-subtitle">
+                <span>
+                  Please fill in the form below and you will be contacted by one
+                  of our
+                </span>
+                <span>professional business experts.</span>
+              </div>
+            </div>
+            <div className="registry-submit">
+              <div className="form-input">
+                <div className="form-input-title">Topic</div>
+                <div className="form-input-field">
+                  <select>
+                    <option></option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-input required-field">
+                <div className="form-input-title">First Name</div>
+                <div className="form-input-field">
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="form-input required-field">
+                <div className="form-input-title">Last Name</div>
+                <div className="form-input-field">
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="form-input required-field">
+                <div className="form-input-title">Email</div>
+                <div className="form-input-field">
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="form-input">
+                <div className="form-input-field">
+                  <button>Register</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    )
-
+  );
 }
-export default Homepage
+export default Homepage;
